cae702f07191df723d01814cea66d3b358f3a50a
Ernesto <neto_neto@hotmail.com>
2016-07-14 16:36:04 -0500

    Add very basic ping and pong exercise

---
+++ public/ping.php
@@ -0,0 +1,23 @@
+<?php
+
+function pageController() {
+    $data = [];
+    $data['currentValue'] = (isset($_GET['count'])) ? $_GET['count'] : 0;
+    return $data;
+}
+
+extract(pageController());
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>PONG</title>
+</head>
+<body>
+<h1>Score: <?= $currentValue; ?></h1>
+<p><a href="/pong.php?count=<?= $currentValue + 1; ?>">HIT</a></p>
+<p><a href="/pong.php?count=0">MISS</a></p>
+</body>
+</html>
