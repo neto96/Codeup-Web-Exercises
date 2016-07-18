6b06d175727221e5a320283d3ec18e5fbc2287f2
Ernesto <neto_neto@hotmail.com>
2016-07-18 10:19:58 -0500

    Add login exercise with authorized page

---
+++ public/authorized.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        body {
+            background-image: url("/img/funky.gif");
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="col-md-8 col-md-offset-4">AUTHORIZED</h1>
+    </div>
+</div>
+<img src="img/woohoo.gif" class="col-md-6 col-md-offset-3">
+</body>
+</html>
---
+++ public/login.php
@@ -0,0 +1,61 @@
+<?php
+$username = isset($_POST['username']) ? $_POST['username'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+if (($username == 'guest') && ($password == 'password')) {
+    header('Location: authorized.php');
+    exit;
+} elseif (($username !== 'guest' && $username !== '') || ($password !== 'password' && $password !== '')) {
+    $message = "Login failed, please try again";
+    $image = "/img/doh.gif";
+}
+
+?>
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+        body {
+            background-image: url("/img/bwbrick.jpg");
+        }
+        .col-md-4 {
+            margin-top: 10%;
+        }
+    </style>
+</head>
+<body>
+<div class="col-md-4 col-md-offset-4">
+    <div class="login-panel panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Please Sign In</h3>
+        </div>
+        <div class="panel-body">
+            <?php if (isset($message)) : ?>
+            <div class="alert alert-danger" role="alert">
+                <h1><?= $message; ?></h1>
+            </div>
+            <?php endif; ?>
+            <form method="POST" role="form">
+                <fieldset>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Username" name="username" type="username" autofocus="">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                    </div>
+                    <input type="submit" name="submit" id="submit" value="Login" class="btn btn-primary pull-right">
+                </fieldset>
+            </form>
+            <?php if (isset($image)) : ?>
+            <img src="<?= $image; ?>" class="col-md-6 col-md-offset-3">
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
